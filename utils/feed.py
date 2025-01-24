57ad0e9c044fa0976517b190329b1ff32cd7379c
Ramon <143756997+mon-gmx@users.noreply.github.com>
2025-01-24 10:23:58 -0600

    Update feed.py
    Fixing a nested card breaking css on large entries

--- utils/feed.py
+++ utils/feed.py
@@ -32,7 +32,7 @@ def feed_to_dict(feed_url: str, limit: int = 10) -> dict:
                         "title": getattr(entry, "title", "Untitled"),
                         "url": getattr(entry, "link", "No link"),
                         "date": getattr(entry, "published", "Unknown date"),
-                        "summary": entry.summary,
+                        "summary": getattr(entry, "summary", None),
                     }
                 )
     return feed_dict
@@ -42,31 +42,39 @@ def dict_to_markdown(feed_dict: dict) -> str:
     markdown_entry = ""
 
     if feed_dict:
+        if not feed_dict.get("entries", []):
+            return ""
         markdown_entry = (
-            f"<div class=\"container-lg\" align=\"right\">"
-            f"<h2>{feed_dict['title']}\n"
-            f"<a href=\"{feed_dict['url']}\">&#x1F517;</a>\n</h2>\n"
-            f"<h3>Description: {feed_dict['subtitle']}</h3>\n"
-            f"<i>{feed_dict['summary']}</i><br/>"
-            f"</div>"
+            f"<div class=\"container-lg\" align=\"right\">\n"
+            f"  <h2>{feed_dict['title']}\n"
+            f"  <a href=\"{feed_dict['url']}\">&#x1F517;</a>\n</h2>\n"
+            f"  <h3>Description: {feed_dict['subtitle']}</h3>\n"
+            f"  <i>{feed_dict['summary']}</i><br/>\n"
+            f"</div>\n"  # container for feed
         )
 
         accordion_id = str(uuid4())
         markdown_entry += f"<div id=\"accordion_{accordion_id}\" role=\"tablist\">\n"
         for entry in enumerate(feed_dict["entries"]):
-            markdown_entry += f"<div class=\"card\">"
-            markdown_entry += f"<div class=\"card-header\" id=\"heading_{entry[0]}\">"
-            markdown_entry += f"<h2 class=\"mb-0\">"
+            if not entry:
+                continue
+            markdown_entry += f"  <div class=\"card\">\n"
+            markdown_entry += f"    <div class=\"card-header\" id=\"heading_{entry[0]}\">\n"
+            markdown_entry += f"      <h2 class=\"mb-0\">\n"
             markdown_entry += (
-                f"<button class=\"btn text-left font-weight-bold\" type=\"button\" data-toggle=\"collapse\" "
+                f"        <button class=\"btn text-left font-weight-bold\" type=\"button\" data-toggle=\"collapse\" "
                 f"data-target=\"#panel_{entry[0]}\" data-parent=\"#accordion_{accordion_id}\">\n"
             )
-            markdown_entry += f"{entry[1]['title']}<a href=\"{entry[1]['url']}\">&#x1F517;</a><br/>\n"
-            markdown_entry += f"<i>{entry[1]['date']}</i>\n"
-            markdown_entry += f"</button>\n</h2>\n</div>\n\n"
-            markdown_entry += f"<div id=\"panel_{entry[0]}\" class=\"collapse\">\n"
-            markdown_entry +=f"<div class=\"card-body\">"
-            markdown_entry +=f"{entry[1]['summary']}\n"
-            markdown_entry += "</div>\n</div><br/>\n"
-        markdown_entry += "</div>\n"
+            markdown_entry += f"        {entry[1]['title']}<a href=\"{entry[1]['url']}\">&#x1F517;</a><br/>\n"
+            markdown_entry += f"          <i>{entry[1]['date']}</i>\n"
+            markdown_entry += f"        </button>\n"
+            markdown_entry += f"      </h2>\n"
+            markdown_entry += f"    </div>\n"  # card header
+            markdown_entry += f"    <div id=\"panel_{entry[0]}\" class=\"collapse\">\n"
+            markdown_entry += f"      <div class=\"card-body\">\n"
+            markdown_entry += f"        {entry[1]['summary']}\n"
+            markdown_entry += f"      </div>\n"  # card body
+            markdown_entry += f"    </div>\n"  # panel
+            markdown_entry += f"  </div>\n"  # card
+    markdown_entry += f"</div>\n"  # accordion
     return markdown_entry
